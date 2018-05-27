b09f4dbda08c41257b278f6fccb3268a7ce79ba8
annette.beatty@gmail.com <annette.beatty@gmail.com>
2018-05-26 19:32:00 -0700

    Added web formatting

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+/* Javascript */
+
+
+/*
+countGuess(GuessArray, UserGuess)
+{
+    UserGuess.count++;
+    if (UserGuess.count > 8) 
+    {                              // Out of guesses
+        return -1;
+    }
+
+    // Add the guess to the array of guesses to keep track
+    GuessArray = GuesArray + UserGuess; 
+}
+*/
